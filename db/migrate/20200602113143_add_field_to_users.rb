6297cf6604451ecbb94694d9a09413e66cdb317a
yue yin <yinyue626@gmail.com>
2020-06-02 14:31:57 +0200

    usermodel_add_field

---
+++ db/migrate/20200602113143_add_field_to_users.rb
@@ -0,0 +1,9 @@
+class AddFieldToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :language, :string
+    add_column :users, :description, :text
+  end
+end
