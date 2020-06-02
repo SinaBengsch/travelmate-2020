86fd8d1dbde2070fa309954890d6d1088c61fd49
Matthew Zimel <40475484+matthewnzimel@users.noreply.github.com>
2020-06-02 10:16:31 -0400

    Update activities_controller.rb

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -12,18 +12,4 @@ class ActivitiesController < ApplicationController
     @activity = Activity.find(params[:id])
     # @booking = Booking.new
   end
-
-  # def new
-  #   @bike = Bike.new
-  # end
-
-  # def create
-  #   @bike = Bike.new(bikes_params)
-  #   @bike.user = current_user
-  #   if @bike.save
-  #     redirect_to bikes_path
-  #   else
-  #     render :new
-  #   end
-  # end
 end
