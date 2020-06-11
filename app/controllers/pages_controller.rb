ede8d993c6ca751c1280fa5b624c2266156fc256
Philipp Iversen <p.iversen99@yahoo.de>
2020-06-11 17:13:36 +0200

    anchor and correct cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @activities = Activity.all.sample(3)
+    @activities = Activity.all.sample(4)
   end
 
   def dashboard
